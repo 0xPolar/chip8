304e056ac8d2ac8276720729b8588ac3f67c06bc
Drew <oaklap@gmail.com>
2026-03-25 13:46:02 +0000

    added SHRVx instruction

--- src/internal/cpu.rs
+++ src/internal/cpu.rs
@@ -1,6 +1,6 @@
 use std::fmt::Error;
 
-use crate::internal::{cpu, display::Display, keypad::Keypad};
+use crate::internal::{display::Display, keypad::Keypad};
 
 pub struct CPU {
     regs: [u8; 16],
@@ -48,7 +48,7 @@ impl CPU {
 
         match (opcode & 0xF000) {
             0x0000 => match opcode {
-                0x00E0 => display.clear(),
+                0x00E0 => CPU::CLS(display),
                 0x00EE => CPU::RET(self),
 
                 _ => return Err(format!("Unknown Opcode: {:#06x}", opcode)),
@@ -84,64 +84,87 @@ impl CPU {
     }
 
     // Skip next instruction if Vx = kk.
-    fn SEVx(cpu: &mut CPU, register: usize, payload: u8) {
-        if cpu.regs[register] == payload {
+    fn SEVx(cpu: &mut CPU, rX: usize, payload: u8) {
+        if cpu.regs[rX] == payload {
             cpu.program_counter += 2;
         }
     }
 
     // Skip next instruction if Vx != kk.
-    fn SNEVx(cpu: &mut CPU, register: usize, payload: u8) {
-        if !(cpu.regs[register] != payload) {
+    fn SNEVx(cpu: &mut CPU, rX: usize, payload: u8) {
+        if !(cpu.regs[rX] != payload) {
             cpu.program_counter += 2;
         }
     }
 
     // Skip next instruction if Vx = Vy
-    fn SEVxVy(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        if cpu.regs[register_x] == cpu.regs[register_y] {
+    fn SEVxVy(cpu: &mut CPU, rX: usize, rY: usize) {
+        if cpu.regs[rX] == cpu.regs[rY] {
             cpu.program_counter += 2;
         }
     }
 
     // Set Vx to KK
-    fn LDVx(cpu: &mut CPU, register: usize, payload: u8) {
-        cpu.regs[register] = payload;
+    fn LDVx(cpu: &mut CPU, rX: usize, payload: u8) {
+        cpu.regs[rX] = payload;
     }
 
     // Add kk to Vx and store in Vx
-    fn ADDVx(cpu: &mut CPU, register: usize, payload: u8) {
-        cpu.regs[register] += payload;
+    fn ADDVx(cpu: &mut CPU, rX: usize, payload: u8) {
+        cpu.regs[rX] += payload;
     }
 
-    // Store the value of register Vy in Vx
-    fn LDVxVy(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        cpu.regs[register_x] = cpu.regs[register_y]
+    // Store the value of rX Vy in Vx
+    fn LDVxVy(cpu: &mut CPU, rX: usize, rY: usize) {
+        cpu.regs[rX] = cpu.regs[rY]
     }
 
     // Set Vx to Vx bitwise or'ed Vy
-    fn ORVxVy(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        cpu.regs[register_x] = cpu.regs[register_x] | cpu.regs[register_y];
+    fn ORVxVy(cpu: &mut CPU, rX: usize, rY: usize) {
+        cpu.regs[rX] = cpu.regs[rX] | cpu.regs[rY];
     }
 
     // Set Vx to Vx btwise and'ed Vy
-    fn ANDVxVy(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        cpu.regs[register_x] = cpu.regs[register_x] & cpu.regs[register_y];
+    fn ANDVxVy(cpu: &mut CPU, rX: usize, rY: usize) {
+        cpu.regs[rX] = cpu.regs[rX] & cpu.regs[rY];
     }
 
     // Set Vx to Vx bitwise xor'ed Vy
-    fn XORVxVy(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        cpu.regs[register_x] = cpu.regs[register_x] ^ cpu.regs[register_y];
+    fn XORVxVy(cpu: &mut CPU, rX: usize, rY: usize) {
+        cpu.regs[rX] = cpu.regs[rX] ^ cpu.regs[rY];
     }
 
     // Set Vx to Vx + Vy, Vf to carry
-    fn CRRYADD(cpu: &mut CPU, register_x: usize, register_y: usize) {
-        let sum: u16 = cpu.regs[register_x] as u16 + cpu.regs[register_y] as u16;
+    fn CRRYADD(cpu: &mut CPU, rX: usize, rY: usize) {
+        let sum: u16 = cpu.regs[rX] as u16 + cpu.regs[rY] as u16;
 
         if sum > 255 {
             cpu.regs[0xF] = 1;
         }
 
-        cpu.regs[register_x] = sum as u8;
+        cpu.regs[rX] = sum as u8;
+    }
+
+    // If Vx > Vy, then VF is set to 1, otherwise 0.
+    // Then Vy is subtracted from Vx, and the results stored in Vx.
+    fn BRWSUB(cpu: &mut CPU, rX: usize, rY: usize) {
+        if cpu.regs[rX] > cpu.regs[rY] {
+            cpu.regs[0xF] = 1;
+        } else {
+            cpu.regs[0xF] = 0;
+        }
+
+        cpu.regs[rX] = cpu.regs[rX] - cpu.regs[rY];
+    }
+
+    // Set Vx = Vx SHR 1.
+    fn SHRVc(cpu: &mut CPU, rX: usize) {
+        if (cpu.regs[rX] & 0x000F) == 0x01 {
+            cpu.regs[0xF] = 1;
+        } else {
+            cpu.regs[0xF] = 0;
+        }
+
+        cpu.regs[rX] /= 2;
     }
 }
